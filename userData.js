3f7839f05c69bfce12c4d87f4c0ac62a466b2091
Karla G. <k.grajalesluna@gmail.com>
2025-02-05 17:37:28 +0000

    added fuction to tested

---
+++ userData.js
@@ -0,0 +1,7 @@
+// // ================== function addData  ====================
+
+// function addData(userId, data) {
+//   let storedData = JSON.parse(localStorage.getItem(userId)) || [];
+//   storedData.push(data);
+//   localStorage.setItem(userId, JSON.stringify(storedData));
+// }
